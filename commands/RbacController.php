27fb18b9f2350ea4bb8ccee74fa076986b072653
Ruslan Skazkopodatelev <ruslan_air12@MacBook-Air-Ruslan.local>
2019-10-01 16:50:39 +0300

    Add purge action to RbacController
    Modify indexAction of RbacController

--- commands/RbacController.php
+++ commands/RbacController.php
@@ -4,12 +4,32 @@
 namespace app\modules\auth\commands;
 
 
+use Yii;
 use yii\console\Controller;
 
-class RbacController extends  Controller
+
+class RbacController extends Controller
 {
-    public function actionIndex($message = 'hello world from module')
+    public function actionIndex()
     {
-        echo $message . PHP_EOL;
+        $auth = Yii::$app->authManager;
+
+        $auth->removeAll();
+
+        $admin = $auth->createRole('admin');
+        $editor = $auth->createRole('editor');
+        $user = $auth->createRole('user');
+
+        $auth->add($admin);
+        $auth->add($editor);
+        $auth->add($user);
+
+
+
+    }
+    public function actionPurge()
+    {
+        Yii::$app->authManager->removeAll();
+        echo 'All RBAC data was purged' . PHP_EOL;
     }
 }
