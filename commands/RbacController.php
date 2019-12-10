c555926f79b7676b140519cf3edd5412be6092c7
skazkin <russkazkin@gmail.com>
2019-12-10 15:13:27 +0300

    Add articles rbac permissions

--- commands/RbacController.php
+++ commands/RbacController.php
@@ -16,6 +16,8 @@ class RbacController extends Controller
 
         $auth->removeAll();
 
+        echo 'All RBAC data was purged...' . PHP_EOL;
+
         $admin = $auth->createRole('admin');
         $editor = $auth->createRole('editor');
         $user = $auth->createRole('user');
@@ -24,7 +26,31 @@ class RbacController extends Controller
         $auth->add($editor);
         $auth->add($user);
 
+        echo 'Users added...' . PHP_EOL;
 
+        $createArticle = $auth->createPermission('createArticle');
+        $createArticle->description = 'Blog module articles creation';
+        $auth->add($createArticle);
+
+        $editOwnArticle = $auth->createPermission('editOwnArticle');
+        $editOwnArticle->description = 'Blog module user articles editing';
+        $auth->add($editOwnArticle);
+
+        $deleteOwnArticle = $auth->createPermission('deleteOwnArticle');
+        $deleteOwnArticle->description = 'Blog module user articles deleting';
+        $auth->add($deleteOwnArticle);
+
+        $editAllArticles = $auth->createPermission('editAllArticles');
+        $editAllArticles->description = 'Blog module articles deleting';
+        $auth->add($editAllArticles);
+
+        $adminArticlesPermissions = $auth->createPermission('adminArticlesPermissions');
+        $adminArticlesPermissions->description = 'Blog module admin articles permissions';
+        $auth->add($adminArticlesPermissions);
+
+        echo 'Permissions added...' . PHP_EOL;
+
+        echo 'All tasks complete' . PHP_EOL;
 
     }
     public function actionPurge()
